cea6bb6bb3836983970dee3c6f4880545b811af3
Agunivers7 <herokuone7@gmail.com>
2022-10-16 09:51:21 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,8 +1,8 @@
 
-
+import telebot
 token = "5546235142:AAGUa1r-5_N1joxXLWDvcjf8uUJjs2SgShA"
 
-bot = telebot.TeleBot("token")
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
